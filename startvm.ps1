e851e35c2bbcb6b3c0c17a890c5dd7d2a797cd73
Pedro Catana <80524795+pedrocatana@users.noreply.github.com>
2023-01-04 19:59:28 +0000

    Update startvm.ps1

--- startvm.ps1
+++ startvm.ps1
@@ -1,11 +1,10 @@
-# Iniciar as máquinas virtuais
+$vm1 = Get-VM -Name "VM1" // Definir a máquina 1 como $vm1
+$vm2 = Get-VM -Name "VM2" // Definir a máquina 2 como $vm2
+$vm3 = Get-VM -Name "VM3" // Definir a máquina 3 como $vm3
+$vm4 = Get-VM -Name "VM4" // Definir a máquina 4 como $vm4
 
-$vm1 = Get-VM -Name "VM1"
-$vm2 = Get-VM -Name "VM2"
-$vm3 = Get-VM -Name "VM3"
-$vm4 = Get-VM -Name "VM4"
 
-Start-VM -VM $vm1
-Start-VM -VM $vm2
-Start-VM -VM $vm3
-Start-VM -VM $vm4
+Start-VM -VM $vm1 // Iniciar a VM1
+Start-VM -VM $vm2 // Iniciar a VM2
+Start-VM -VM $vm3 // Iniciar a VM3
+Start-VM -VM $vm4 // Iniciar a VM4
