fafff51b2026365ceb7a3e5ba3216a3880561ddc
Pedro Catana <80524795+pedrocatana@users.noreply.github.com>
2023-01-08 21:29:16 +0000

    Add files via upload

---
+++ connectvms.ps1
@@ -0,0 +1,4 @@
+vmconnect DESKTOP-0J01RE5 "VM1 - Ubuntu 22.04"
+vmconnect DESKTOP-0J01RE5 "VM2 - Windows 10"
+vmconnect DESKTOP-0J01RE5 "VM3 - Windows 7"
+vmconnect DESKTOP-0J01RE5 "VM4 - Mint 21.1"
