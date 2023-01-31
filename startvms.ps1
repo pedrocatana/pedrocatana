962e5b9b6b8dadfd019c5fdfbd8817d69a50fc7c
Pedro Catana <80524795+pedrocatana@users.noreply.github.com>
2023-01-31 17:33:35 +0000

    Delete startvms.ps1

--- startvms.ps1
+++
@@ -1,9 +0,0 @@
-$vm1 = Get-VM -Name "VM1 - Ubuntu 22.04"
-$vm2 = Get-VM -Name "VM2 - Windows 10"
-$vm3 = Get-VM -Name "VM3 - Windows 7"
-$vm4 = Get-VM -Name "VM4 - Mint 21.1"
-
-Start-VM -VM $vm1
-Start-VM -VM $vm2
-Start-VM -VM $vm3
-Start-VM -VM $vm4
